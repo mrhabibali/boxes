64498370f2c508d01cdb388436c87a9c0211a06e
mrhabibali <habibali92@hotmail.com>
2023-05-12 17:01:09 +0200

    React project about boxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import boxes from "./boxes";
+import Box from "./components/Box";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [squares, setsquares] = React.useState(boxes);
+
+  const sqaureElements = squares.map((sqaure) => {
+    return <Box key={sqaure.id} id={sqaure.id} on={sqaure.on} />;
+  });
+
+  return <div>{sqaureElements}</div>;
 }
 
 export default App;
